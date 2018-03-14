53444eb9458173b088c38d9b2dda57143764867e
Ian <imatthews009@gmail.com>
2018-03-14 15:31:55 -0700

    created new react app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import Twit from 'twit';
+var T = new Twit({
+  consumer_key:         'HlSvxCHIVJccYSBDV1oYB2E0H',
+  consumer_secret:      'iNSEIm97IqsWMCcfZ01xzP1nAOY3QPAtEYozbnScYKmJ9lruuP',
+  access_token:         '2387332320-naFN1qp9xEzZfieFH9zXnYiGzO7rQqx0HW0Zz9V',
+  access_token_secret:  'LfjjdaNruMReO0BFryPrjTIMu6gbBSVCyH5Iz4qQRzDm6'
+});
+
+class App extends Component {
+  state = {
+
+  }
+
+  componentWillMount() {
+    var params = {
+      q: 'rainbow',
+      count: 2
+    }
+    T.get('search/tweets', params,  function (err, data, response) {
+      console.log(data)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
